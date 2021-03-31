f20ada7f7d5c2be8102e92221df9a89d6c810aaf
Muhammad Waqas <bsse.waqas@gmail.com>
2021-03-31 16:39:59 +0500

    check fixations

--- app/src/main/java/com/example/volleylogin/MainActivity.java
+++ app/src/main/java/com/example/volleylogin/MainActivity.java
@@ -1,24 +1,26 @@
 package com.example.volleylogin;
+
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import com.android.volley.*;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-
-import java.util.HashMap;
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
 
 
 public class MainActivity extends AppCompatActivity {
     Button b1;
     EditText etuserName;
     EditText etpassword;
-    private static final String URL ="https://apis.martcart.pk/";
+    private static final String URL = "https://api.martcart.pk/api/v1/user/login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,42 +29,54 @@ public class MainActivity extends AppCompatActivity {
         etuserName = findViewById(R.id.etuserName);
         etpassword = findViewById(R.id.etpassword);
         b1 = findViewById(R.id.btnlogin);
+
+        final JSONObject request = new JSONObject();
+        try {
+            request.put("userName", etuserName.getText().toString());
+            request.put("password", etpassword.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST, URL, request, new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
-
-                        User user = new User(response);
-                        Toast.makeText(MainActivity.this, user.getToken(), Toast.LENGTH_SHORT).show();
-
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Toast.makeText(MainActivity.this, response.getString("token"), Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        // As of f605da3 the following should work
+                        NetworkResponse response = error.networkResponse;
+                        if (error instanceof ServerError && response != null) {
+                            try {
+                                String res = new String(response.data,
+                                        HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                                // Now you can use any deserializer to make sense of data
+                                JSONObject obj = new JSONObject(res);
+                            } catch (UnsupportedEncodingException e1) {
+                                // Couldn't properly decode data to string
+                                e1.printStackTrace();
+                            } catch (JSONException e2) {
+                                // returned data is not JSONObject?
+                                e2.printStackTrace();
+                            }
+                        }
                         Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
 
                     }
-                })
-                {
-
-                    public Map<String, String> getHeaders() {
-                        Map<String, String> headers = new HashMap<>();
-                        headers.put("Content-Type: application/json;charset=UTF-8","api/v1/user/login");
-                        return headers;
-                    }
-
-                    @Override
-                    protected Map<String, String> getParams() {
-                        Map<String, String> params = new HashMap<>();
-                        params.put("userName", String.valueOf(etuserName));
-                        params.put("password", String.valueOf(etpassword));
-                        return params;
-                    }
+                }) {
                 };
                 RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-                queue.add(request);
+                queue.add(jsonObjReq);
             }
 
         });
